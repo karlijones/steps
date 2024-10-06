33512b0b4f9f5e72ecb0a6ed0b110b5420e57de7
karlijones <karlijones1984@gmail.com>
2024-10-06 13:22:41 -0500

    step 1 array

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@
 ];
  
 export default function App() {
+  const step = 1;
+
   return <div className="steps">
     <div className="numbers">
       <div className="active">1</div>
@@ -12,7 +14,7 @@ export default function App() {
       <div>3</div>
     </div>
   
-    <p className="message">Hello</p>
+    <p className="message">Step {step}: {messages[step - 1]}</p>
     <div className="buttons"></div>
     <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Previous</button>
     <button style={{backgroundColor: '#7950f2', color: '#fff'}}>Next</button>
