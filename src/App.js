249f8dc1ac57ca926bd9ae973b4c5b6a4b3add8d
karlijones <karlijones1984@gmail.com>
2024-10-06 13:31:49 -0500

    ternary operators added for steps 2 and 3

--- src/App.js
+++ src/App.js
@@ -10,8 +10,8 @@ export default function App() {
   return <div className="steps">
     <div className="numbers">
       <div className={`${step >= 1 ? "active" : "" }`}>1</div>
-      <div>2</div>
-      <div>3</div>
+      <div className={`${step >= 2 ? "active" : "" }`}>2</div>
+      <div className={`${step >= 3 ? "active" : "" }`}>3</div>
     </div>
   
     <p className="message">
