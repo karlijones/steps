2bbc4ea944f6f6bdf30ca1221063f85e90c442a0
karlijones <karlijones1984@gmail.com>
2024-10-05 22:12:39 -0500

    divs added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div className="steps">Hello React</div>;
+  return <div className="steps">
+    <div className="numbers">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </div>
+  
+  </div>;
 }
