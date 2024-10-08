3831eddae4f654a620c64499a0d0e1de434ac137
karlijones <karlijones1984@gmail.com>
2024-10-07 20:08:38 -0500

    setStep

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 ];
  
 export default function App() {
-  useState 
+  const [step, setStep] = useState(1);
 
   function handlePrevious() {
     alert("Previous");
