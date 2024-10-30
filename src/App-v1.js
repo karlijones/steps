e77df5601490997727d4a882314254c7649e25a1
karlijones <karlijones1984@gmail.com>
2024-10-30 15:49:28 -0500

    StepMessage function

--- src/App-v1.js
+++ src/App-v1.js
@@ -44,10 +44,10 @@ function Steps() {
           <div className={step >= 2 ? "active" : "" }>2</div>
           <div className={step >= 3 ? "active" : "" }>3</div>
         </div>
-      
-        <p className="message">
-          Step {step}: {messages[step - 1]}
-        </p>
+
+        <StepMessage step={step}>
+        {messages[step - 1]}
+        </StepMessage>
 
         <div className="buttons">
         <Button 
@@ -69,6 +69,14 @@ function Steps() {
   );
 }
 
+function StepMessage(step, children) {
+    return (
+    <div className="message">
+          <h3>Step {step}</h3>
+          {children}
+    </div>
+    );
+}
 
 function Button({textColor, bgColor, onClick, children }) {
     return (
