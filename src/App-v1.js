22dc2dcb0ce657c2598a318900ea5b7fb3f13058
karlijones <karlijones1984@gmail.com>
2024-10-30 16:28:26 -0500

    restructuring

--- src/App-v1.js
+++ src/App-v1.js
@@ -29,29 +29,27 @@ function Steps() {
   function handlePrevious() {
     if (step > 1) setStep((s) => s - 1);
   }
-
+  
   function handleNext() {
     if (step < 3) {
-        setStep(step + 1);
-        }
+        setStep((s) => s + 1);
     }
+  }
 
-
-  return (
-    <div>
-      <button className="close" onClick={()=>setIsOpen
-        ((is) => !is)}>
-         &times;
+    return (
+      <div>
+        <button className="close" onClick={()=> setIsOpen
+            ((is) => !is)}>
+            &times;
         </button>
-    
 
-    { isOpen && (
-      <div className="steps">
-        <div className="numbers">
-          <div className={step >= 1 ? "active" : "" }>1</div>
-          <div className={step >= 2 ? "active" : "" }>2</div>
-          <div className={step >= 3 ? "active" : "" }>3</div>
-        </div>
+        {isOpen && (
+          <div className="steps">
+            <div className="numbers">
+            <div className={step >= 1 ? "active" : "" }>1</div>
+            <div className={step >= 2 ? "active" : "" }>2</div>
+            <div className={step >= 3 ? "active" : "" }>3</div>
+          </div>
 
         <StepMessage step={step}>
          {messages[step - 1]}
